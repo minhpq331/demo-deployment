9e53b46fb6bccb2da9f561843e8f7fcffbe2c105
Pham Quang Minh <phamquangminh@gapo.com.vn>
2020-09-11 16:14:02 +0700

    add config example

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const os = require('os')
+const fs = require('fs')
 const app = express()
 
 const port = parseInt(process.env.PORT, 10) || 0
@@ -20,6 +21,28 @@ if (!message) {
 app.get('/', (req, res) => {
   res.send(`Hello, I'm running on ${os.hostname()} with version ${version}!`)
   res.send(`Message: ${message}`)
+})
+
+app.get('/config-env', (req, res) => {
+  res.send(`Loaded config from env: ${process.env.CONFIG_ENV}`)
+})
+
+app.get('/secret-env', (req, res) => {
+  res.send(`Loaded secret from env: ${process.env.SECRET_ENV}`)
+})
+
+app.get('/config-file', (req, res) => {
+  res.send(`Loaded config from file:`)
+  fs.readFile('./config.json', 'utf8', function(err, contents) {
+    res.send(contents);
+  });
+})
+
+app.get('/secret-file', (req, res) => {
+  res.send(`Loaded secret from file:`)
+  fs.readFile('./secret.json', 'utf8', function(err, contents) {
+    res.send(contents);
+  });
 })
 
 app.listen(port, () => {
