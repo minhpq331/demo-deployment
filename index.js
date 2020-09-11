456d09b597cc9a0db3521723235864a40318be1c
Pham Quang Minh <phamquangminh@gapo.com.vn>
2020-09-11 14:41:00 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const os = require('os')
+const app = express()
+const port = parseInt(process.env.PORT, 10) || 0
+
+const version = '1.0'
+
+if (!port) {
+	console.log('Missing env PORT!')
+	process.exit(1)
+}
+
+app.get('/', (req, res) => {
+  res.send(`Hello, I'm running on ${os.hostname()} with version ${version}!`)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at port ${port}`)
+})
